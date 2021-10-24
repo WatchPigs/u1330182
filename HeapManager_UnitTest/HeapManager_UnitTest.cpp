ad77cd1244e1579e4f752481282c361adc078338
YexiangZHOU <u1330182@utah.edu>
2021-10-24 13:47:29 -0600

    create unit test project

---
+++ HeapManager_UnitTest/HeapManager_UnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HeapManagerUnitTest
+{
+	TEST_CLASS(HeapManagerUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
